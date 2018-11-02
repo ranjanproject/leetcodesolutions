f0f3d6ac761279929d0b650f089ad6b133da5b92
ranjanproject <36195949+ranjanproject@users.noreply.github.com>
2018-11-02 12:18:23 +0000

    Sync LeetCode submission - Arranging Coins (cpp)

---
+++ leetcodesolutions/problems/arranging_coins/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+      long long i;
+        
+        for(i=0;(i*(i+1))/2<=n;i++){
+        }
+        return i-1;
+    }
+               
+    
+};
