de001afe802ced7f17449f291edd86f3d509bfcb
ranjanproject <36195949+ranjanproject@users.noreply.github.com>
2018-11-08 17:43:53 +0000

    Sync LeetCode submission - Bitwise AND of Numbers Range (cpp)

---
+++ leetcodesolutions/problems/bitwise_and_of_numbers_range/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+       int k = 0;
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            k++;
+        }
+        return m << k;
+    }
+};
