2a5f509bcf6f18e1cd837914bebb1b93338324d7
ranjanproject <36195949+ranjanproject@users.noreply.github.com>
2023-07-21 12:48:51 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- leetcodesolutions/problems/merge_two_sorted_lists/solution.cpp
+++ leetcodesolutions/problems/merge_two_sorted_lists/solution.cpp
@@ -3,39 +3,50 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *head=NULL,*temp;
-        if(!l1&&!l2)return NULL;
-        if(l1==NULL && l2!=NULL)return l2;
-        if(l1!=NULL && l2==NULL)return l1;
-        if(l1->val<l2->val){
-            head=l1;
-            l1=l1->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head, *temp;
+
+        if(list1 != NULL && list2 != NULL){
+          if(list1->val <= list2->val){
+               head = list1;
+               temp = head;
+               list1 = list1->next;
+          }else{
+               head = list2;
+               temp = head;
+               list2 = list2->next;
+          }   
+        }else if(list1 == NULL){
+               return list2;
+        }else{
+            return list1;
         }
-        else{
-            head=l2;
-            l2 = l2->next;
+
+        while(list1 != NULL && list2 != NULL){
+          if(list1->val <= list2->val){
+                temp->next = list1;
+                temp = list1;
+                list1 = list1 -> next;
+          }else{
+              temp->next = list2;
+              temp = list2;
+              list2 = list2->next;
+          }   
         }
-        temp = head;
-        while(l1 && l2){
-            // cout<<temp->val<<endl;
-            if(l1->val < l2->val){
-                temp->next = l1;
-                l1 = l1->next;
-            }
-            else{
-                temp->next = l2;
-                l2 = l2->next;
-            }
-            temp = temp->next;
+
+        if(list1 == NULL){
+           temp -> next = list2;
+        }else{
+           temp -> next = list1;
         }
-        if(l1!=NULL)temp->next=l1;
-        if(l2!=NULL)temp->next=l2;
-        return head;
+
+        return head;        
     }
 };
